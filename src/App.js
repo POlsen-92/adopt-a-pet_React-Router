cfdfa9f8eec6aef6ee75a5b40a8317203cff6e65
Paige Olsen <88565036+POlsen-92@users.noreply.github.com>
2021-12-22 15:18:36 -0800

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import HomePage from './pages/home';
+import SearchPage from './pages/search';
+import PetDetailsPage from './pages/detail';
+import PetDetailsNotFound from './pages/petDetailsNotFound';
+import Navigation from './components/navigation';
+
+import { BrowserRouter as Router } from 'react-router-dom';
+
+function App() {
+  return (
+    <div>
+      <Navigation />
+      <HomePage />
+    </div>
+  );
+}
+
+export default App;
